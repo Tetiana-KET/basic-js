0c411182ddd45f2fb670fa6c410f642e160a8fd6
Tetiana-KET <belangelphone@gmail.com>
2022-10-17 12:29:41 +0300

    encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function encodeLine(s) {
+
+  let result = '';
+  let count =1;
+
+  //loop through all the letters of the string and compare each pair of two consecutive letters:
+  for (let i = 0; i < s.length; i++) {
+
+      if (s[i] === s[i+1]) {
+          count++;
+      } else {// if the current letter s[i] and the next letter s[i+1] is different
+          result = result + (count + s[i]);
+          count = 1;//We also need to reset count
+      }
+      
+  }
+  return result.replace(/1/g, '');
 }
 
 module.exports = {
