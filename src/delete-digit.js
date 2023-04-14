6a55a51c9aa50d18d13061173b6c7b0711cfdb05
Tetiana-KET <belangelphone@gmail.com>
2023-04-14 16:57:07 +0300

    solve Delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -9,11 +9,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * For n = 152, the output should be 52
- *
+ * assert.strictEqual(deleteDigit(152), 52);
+ *  assert.strictEqual(deleteDigit(1001), 101);
+ * assert.strictEqual(deleteDigit(10), 1);
+ *assert.strictEqual(deleteDigit(222219), 22229);
+ *  assert.strictEqual(deleteDigit(109), 19);
+ * assert.strictEqual(deleteDigit(342), 42);
+ * 
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const result = [];
+  const str = n.toString();
+  for (let digit of str) {
+    result.push(+str.replace(digit, ''));
+  }
+  return Math.max(...result)
 }
 
 module.exports = {
