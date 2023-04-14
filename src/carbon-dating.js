f0583251bdf81ce582a846d875c2a405c060e63c
Tetiana-KET <belangelphone@gmail.com>
2023-04-14 15:03:59 +0300

    feat: Recursive depth calculator

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,19 +17,27 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-//Math.log() - Метод Math.log() возвращает натуральный (по основанию e) логарифм числа
-//логарифм — это степень, в которую нужно возвести a для получения b
-//периодом полураспада реакции и записывается как t 1 /2 
-//t 1 /2 = 0,693/ k .
-//формула расчета времени 
-// time = ln ( N0 / N ) / k.....
-// N0 это начальное значение полураспада, то есть MODERN_ACTIVITY, 
-// N - это то что есть сейчас, то есть sampleActivity......
-// k рассчитывается по формуле 0,693 / период полураспада HALF_LIFE_PERIOD
-//проверить условия, сделать округление и готово)))))
+function dateSample(sampleActivity) {
+  //Math.log() - Метод Math.log() возвращает натуральный (по основанию e) логарифм числа
+  //логарифм — это степень, в которую нужно возвести a для получения b
+  //периодом полураспада реакции и записывается как t 1 /2 
+  //t 1 /2 = 0,693/ k .
+  //формула расчета времени 
+  // time = ln ( N0 / N ) / k.....
+  // N0 это начальное значение полураспада, то есть MODERN_ACTIVITY, 
+  // N - это то что есть сейчас, то есть sampleActivity......
+  // k рассчитывается по формуле 0,693 / период полураспада HALF_LIFE_PERIOD
+  //проверить условия, сделать округление и готово)))))
+  //Function parameter sampleActivity is a string. Calculated sample age must be number.
+  //Age must be integer. Age must be rounded up (ceiling). 
+  //In case of wrong input parameter type or inadequate activity value or absence of argument function must return false.
+  //const MODERN_ACTIVITY = 15;
+  //const HALF_LIFE_PERIOD = 5730;
 
-  // if (!(typeof sampleActivity == 'string')) return false;
+  if (!(typeof sampleActivity == 'string') || Number.isNaN(+sampleActivity) || sampleActivity<=0) {
+    return false;
+  }
+
 
  
 }
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -10,12 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
- *
+ * calculateDepth method must pass the given array recursively. Depth of a flat array is 1. 
+ * Method must correctly work with arrays that contain no elements or contain empty arrays.
  */
+// function list_depth(arr) {
+//   let n = 1;
+//   for (i of arr) {
+//     if (Array.isArray(i)) {
+//       n += list_depth(i);
+//       console.log(n);
+//     }
+//   }
+//   return n
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    for (let item of arr) {
+      if ( Array.isArray(item))  {
+        const newArr = arr.flat();
+        this.calculateDepth(newArr);
+        depth ++
+      }
+    }
+    return depth;
   }
 }
 
