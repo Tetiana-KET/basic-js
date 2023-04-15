3a417bdf4db09296638e53393942a4dc93a06534
Tetiana-KET <belangelphone@gmail.com>
2023-04-15 16:37:40 +0300

    feat: start task chaining

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,25 +2,40 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
+ *     base requirements
+      - chaining works!
+      - throws an Error with message "You can't remove incorrect link!" on trying to remove wrong link
+      - functional requirements
+      - function returns correct values
+      - removeLinks works correctly
  * 
  */
 const chainMaker = {
+
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    // throw new Error("You can't remove incorrect link!");
+    this.chain.splice(position, 1)
+    return this;
+
+
   },
+  
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
